311f32c038c07fb29b6e980e93ec9554995c23b9
mavis0 <646357406@qq.com>
2020-06-04 23:56:16 +0800

    day34-36 30%

--- day34-36/bar.js
+++ day34-36/bar.js
@@ -2,10 +2,14 @@ function drawHistogram(data) {
     var svg = document.getElementById('svg');
     drawAxis(30, 250, 30, 451);
     drawAxis(30, 450, 600, 450);
-
+    drawOne(100, 500, 100, 200);
+    // drawOne(60, 350, 10, 100);
+    for (let i = 0; i < data.length; i++) {
+        drawOne(60 + i * 40, 450 - data[i], 10, data[i]);
+    }
 }
 
-function drawAxis(x1, y1, x2, y3) {
+function drawAxis(x1, y1, x2, y2) {
     var svg = document.getElementById('svg');
     var line = document.createElementNS("http://www.w3.org/2000/svg", "line");
     line.setAttribute("x1", x1);
@@ -13,6 +17,7 @@ function drawAxis(x1, y1, x2, y3) {
     line.setAttribute("x2", x2);
     line.setAttribute("y2", y2);
     line.setAttribute("style", "stroke: black; stroke-width: 2;")
+    svg.appendChild(line);
 }
 
 function drawOne(x, y , w, h) {
